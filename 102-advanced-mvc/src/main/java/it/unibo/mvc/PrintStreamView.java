82f731fb884f95c91a98e6cf6689247ba2978193
Michele Casamenti <michele.casamenti@studio.unibo.it>
2024-11-25 12:29:57 +0100

    commented one line

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/PrintStreamView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/PrintStreamView.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 /**
  * This class implements a view that can write on any PrintStream.
